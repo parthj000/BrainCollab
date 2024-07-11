9e03a1202b4df2d173eb2cf368d7eadd10c63cd7
Nitish Singh <ns184466655@gmail.com>
2024-07-11 10:26:02 +0530

    weak fix

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import WelcomePage from './welcome'
-import MyCalendar from './calendar.header'
+import MyCalendar from './calendar'
 
 const App = () => {
   return (
